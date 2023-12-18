b19fb99ee1faf0024326f0f5cedf3c3172ff3a87
iloveTrap <44522257+iloveTrap@users.noreply.github.com>
2023-12-18 15:10:49 +0300

    Update BaseApp.py
    Обновление ссылки на тестируемый товар с целью улучшения демонстрации функциональности программы.

--- Автоматизация позитивного тест-кейса (Задание 2)/Favorites_button_(Selenium)/BaseApp.py
+++ Автоматизация позитивного тест-кейса (Задание 2)/Favorites_button_(Selenium)/BaseApp.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.base_url = "https://www.avito.ru/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363"
+        self.base_url = "https://www.avito.ru/yoshkar-ola/knigi_i_zhurnaly/kniga_how_to_read_pattern_3597736665?slocation=633540"
 
     def find_element(self, locator, time=10):
         return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
